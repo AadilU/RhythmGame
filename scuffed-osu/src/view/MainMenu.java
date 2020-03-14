c8e361f5a202fde67508095f2f70a76145a94791
corytran <cktran02@gmail.com>
2020-03-14 05:03:25 -0700

    added main menu music

--- scuffed-osu/src/view/MainMenu.java
+++ scuffed-osu/src/view/MainMenu.java
@@ -13,11 +13,17 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
 
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
 public class MainMenu extends JFrame {
 	
 	/**
@@ -42,9 +48,21 @@ public class MainMenu extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * creates the frame (view as main menu)
+	 * plays mp3 audio file located in audio folder
+	 * 
 	 */
 	public MainMenu() {
+		String songName = "audio//carnation.mp3";
+		String pathToMp3 = System.getProperty("user.dir") +"/"+ songName;
+		BasicPlayer player = new BasicPlayer();
+		try {
+		    player.open(new URL("file:///" + pathToMp3));
+		    player.play();
+		} catch (BasicPlayerException | MalformedURLException e) {
+		    e.printStackTrace();
+		}
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1280, 720);
 		getContentPane().setLayout(null);
@@ -54,11 +72,26 @@ public class MainMenu extends JFrame {
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JButton btnStart = new JButton("start");
+
+		/*
+		 *creates start button
+		 *stops music of main menu when pressed
+		 *closes previous frame
+		 * 
+		 */
+		
+		JButton btnStart = new JButton("start"); 
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Song_selectUI frame = new Song_selectUI();
-				frame.setVisible(true);
+				
+				try {
+					player.stop(); 
+				} catch (BasicPlayerException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				Song_selectUI frame = new Song_selectUI(); 
+				frame.setVisible(true); 
 				
 				dispose();
 			}
