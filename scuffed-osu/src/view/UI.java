2b0e3525e62ce983bdf83b50f2bced50aed8bd02
corytran <cktran02@gmail.com>
2020-03-14 01:45:52 -0700

    added GUI toolkit

--- scuffed-osu/src/view/UI.java
+++ scuffed-osu/src/view/UI.java
@@ -1,5 +1,60 @@
 package view;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
 
-public class UI {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+
+public class UI extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 800);
+		getContentPane().setLayout(null);
+		
+		JButton btnStart = new JButton("start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnStart.setBounds(100, 287, 225, 50);
+		getContentPane().add(btnStart);
+	}
 }
+
